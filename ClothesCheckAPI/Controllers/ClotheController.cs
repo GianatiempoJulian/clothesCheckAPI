8804f3c7ab5ba614641df219ffd227e986257a57
GianatiempoJulian <gianatiempojulian@gmail.com>
2024-09-10 13:19:58 -0300

    build: first commit! [10-09-24]

---
+++ ClothesCheckAPI/Controllers/ClotheController.cs
@@ -0,0 +1,21 @@
+﻿using ClothesCheckAPI.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClothesCheckAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClotheController : Controller
+    {
+        public async Task <ClotheDTO> GetClothes()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task <ClotheDTO> GetClothe(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClothesCheckAPI/DTOs/ClotheDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ClothesCheckAPI.DTOs
+{
+    public class ClotheDTO
+    {
+        public long Id { get; set; }
+        public long Name { get; set; }
+        public long Type { get; set; }
+        public long Color { get; set; }
+        public long Shade { get; set; }
+    }
+}
